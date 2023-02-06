d3cb92450bf4e5e20d6f3325949193d56758612d
wac <wabaham9@gmail.com>
2023-02-06 11:43:38 +0000

    flask/cor3

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import os
 from flask import Flask
 """ Start a web app that listens to
 any ip and port 5000. Prints Hello HBNB! at /
 """
 
-app = Flask("__name__")
-app.url_map.strict_slashes = False
+from flask import Flask
+app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
-def hello():
-    """Display Hello HBNB!"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
