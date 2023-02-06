f40b109917019afb668ddb76de222e9e82adfae8
wac <wabaham9@gmail.com>
2023-02-06 11:34:17 +0000

    flask/cor2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,5 +72,5 @@ class DBStorage:
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.remove()
+        """ calls the remove method"""
+        self._session.remove()
