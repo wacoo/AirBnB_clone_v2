93c864239b197e7442d77c936f2f7b97ac54bc60
wac <wabaham9@gmail.com>
2023-02-06 12:31:52 +0000

    flask/3 cor

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,21 +15,21 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Display Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display HBNB"""
-    return "HBNB"
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def show_value(text):
     """Display variable value"""
     txt = text.replace('_', ' ')
-    return "C " + txt
+    return 'C ' + txt
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
