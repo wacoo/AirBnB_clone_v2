af46dd31b29298f8d4635a0e760230474227d430
wac <wabaham9@gmail.com>
2023-02-06 12:41:33 +0000

    flask/3 cor

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-Start a web app that listens to
+start a web app that listens to
 any ip and port 5000. Prints:
 Hello HBNB! at /,
 HBNB at /hbnb
 value of text at /c/<text>
-value of text at /python/(<text>)
 """
-
-
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Display Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
@@ -33,11 +33,10 @@ def show_value(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def show_vlaue_python(text='is cool'):
-    """Display variable value"""
-    txt = text.replace('_', ' ')
-    return 'Python ' + txt
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
